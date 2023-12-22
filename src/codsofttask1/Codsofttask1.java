a8479e89a351c7004dc1f64628b3d64a1602fe22
Afshan Noreen <126900173+nooraizafshan@users.noreply.github.com>
2023-12-22 21:09:57 +0500

    codsoft

---
+++ src/codsofttask1/Codsofttask1.java
@@ -0,0 +1,14 @@
+
+package codsofttask1;
+
+
+public class Codsofttask1 {
+
+    
+    public static void main(String[] args) {
+        Numbergame g=new Numbergame();
+        g.setVisible(true);
+        
+    }
+    
+}
